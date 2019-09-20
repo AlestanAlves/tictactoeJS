8c9562bd46a9a2e415c968d5d8b4d163c43345c5
alestan <alestanalves@gmail.com>
2019-09-20 14:34:14 -0300

    Refazendo estrutura e terminando lógica

--- script2.js
+++ script2.js
@@ -1,98 +1,87 @@
-var tabuleiro;
-var board;
-var aviso;
-var jogador;
-var lin, col;
+let jogoDaVelha = new TicTacToeGame();
+jogoDaVelha.start();
 
-function inicia()
-{
- tabuleiro = new Array(3);
- board = document.getElementById('board');
- aviso = document.getElementById('aviso');
- jogador = 1;
+function TicTacToeGame() {
+  let board = new Board();
+  let humanPlayer = new HumanPlayer(board);
+  let computerPlayer = new ComputerPlayer(board);
+  let turn = 0;
 
- for(let i=0 ; i<3 ; i++)
-  tabuleiro[i] = new Array(3);
- 
- for(let i=0; i<3 ; i++)
-  for(let j=0 ; j<3 ; j++)
-   tabuleiro[i][j]=0;
- exibe();
+  this.start = function() {
+    let config = { childList: true };
+    let observer = new MutationObserver(() => takeTurn());
+    board.posicoes.forEach((el) => observer.observe(el, config));
+    takeTurn();
+  }
 
+  function takeTurn() {
+    if (board.checkForjogadaGanhador()) {
+      return;
+    }
+
+    if (turn % 2 === 0) {
+      humanPlayer.takeTurn();
+    } else {
+      computerPlayer.takeTurn();
+    }
+
+    turn++;
+  };
 }
 
-function exibe()
-{
- HTML = '<table  cellpadding="10" border="1">';
- for(let i=0; i<3 ; i++){
-  HTML += '<tr>';
-  for(let j=0 ; j<3 ; j++)
-   if(tabuleiro[i][j]==0)
-    HTML +='<td>  __  </td>';
-   else 
-    if(tabuleiro[i][j]==1)
-     HTML +='<td> X </td>';
-    else
-     HTML +='<td> O </td>';
-  HTML += '</tr>';
- }
- HTML += '</table><br />';
- board.innerHTML = HTML
- 
+function Board() {
+  this.posicoes = Array.from(document.querySelectorAll('.quadr'));
+
+  this.checkForjogadaGanhador = function() {
+    let jogadaGanhador = false;
+
+    let Combinacoesdoganhador = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ];
+
+    let posicoes = this.posicoes;
+    Combinacoesdoganhador.forEach((ganhadorCombo) => {
+      let pos0InnerText = posicoes[ganhadorCombo[0]].innerText;
+      let pos1InnerText = posicoes[ganhadorCombo[1]].innerText;
+      let pos2InnerText = posicoes[ganhadorCombo[2]].innerText;
+      let isganhadorCombo = pos0InnerText !== '' &&
+        pos0InnerText === pos1InnerText && pos1InnerText === pos2InnerText;
+      if (isganhadorCombo) {
+          jogadaGanhador = true;
+          ganhadorCombo.forEach((index) => {
+            posicoes[index].className += ' jogadaGanhador';
+          })
+      }
+    });
+
+    return jogadaGanhador;
+  }
 }
 
-function jogar()
-{
- aviso.innerHTML='Vez do jogador: ' + ((jogador)%2 + 1);
- lin = parseInt(document.getElementById("lin").value)-1;
- col = parseInt(document.getElementById("col").value)-1;
-
- if(tabuleiro[lin][col]==0)
-  if(jogador % 2)
-   tabuleiro[lin][col] = 1;
-  else
-   tabuleiro[lin][col] = -1;
- else{
-  aviso.innerHTML='Campo ja foi marcado!'
-  jogador--;
- }
-
- jogador++;
- exibe();
- checa();
+function ComputerPlayer(board) {
+  this.takeTurn = function() {
+    let availableposicoes = board.posicoes.filter((p) => p.innerText === '');
+    let move = Math.floor(Math.random() * (availableposicoes.length - 0));
+    availableposicoes[move].innerText = 'O';
+  }
 }
 
-function checa()
-{
- var soma;
+function HumanPlayer(board) {
+  this.takeTurn = function() {
+    board.posicoes.forEach(el =>
+      el.addEventListener('click', handleTurnTaken));
+  }
 
- //Linhas
- for(let i=0 ; i<3 ; i++){
-  soma=0;
-  soma=tabuleiro[i][0]+tabuleiro[i][1]+tabuleiro[i][2];
-
-  if(soma==3 || soma==-3)
-   aviso.innerHTML="Jogador " + ((jogador)%2 + 1) + " ganhou! Linha " + (i+1) + " preenchida!";
- }
-
- //Colunas
- for(let i=0 ; i<3 ; i++){
-  soma=0;
-  soma=tabuleiro[0][i]+tabuleiro[1][i]+tabuleiro[2][i];
-
-  if(soma==3 || soma==-3)
-   aviso.innerHTML="Jogador " + ((jogador)%2 + 1) + " ganhou! Coluna " + (i+1) + " preenchida!";
- }
-
- //Diagonal
- soma=0;
- soma = tabuleiro[0][0]+tabuleiro[1][1]+tabuleiro[2][2];
- if(soma==3 || soma==-3)
-   aviso.innerHTML="Jogador " + ((jogador)%2 + 1) + " ganhou! Diagonal preenchida!";
-
- //Diagonal
- soma=0;
- soma = tabuleiro[0][2]+tabuleiro[1][1]+tabuleiro[2][0];
- if(soma==3 || soma==-3)
-   aviso.innerHTML="Jogador " + ((jogador)%2 + 1) + " ganhou! Diagonal preenchida!";
+  function handleTurnTaken(event) {
+    event.target.innerText = 'X';
+    board.posicoes
+      .forEach(el => el.removeEventListener('click', handleTurnTaken));
+  }
 }
